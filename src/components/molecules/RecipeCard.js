7e2a8d9eed383e25fa140748410206d5f71c93c3
Rachmad NH <116181515+Rachmad22@users.noreply.github.com>
2023-03-06 10:36:42 +0700

    [FIX] Eslint

--- src/components/molecules/RecipeCard.js
+++ src/components/molecules/RecipeCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import * as recipeReducer from "../../store/recipe";
+import * as recipeReducer from "@/store/recipe";
 import { useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 
