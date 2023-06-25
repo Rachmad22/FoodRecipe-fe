7a2296b420fe9a07a9aaa59680c594743f3da04f
Rachmad22 <rachmadnur22@gmail.com>
2023-06-25 17:18:54 +0700

    [ADD Google Analytics and Sentry

--- src/App.js
+++ src/App.js
@@ -13,6 +13,29 @@ import Logout from "./pages/Logout";
 import store from "./store/Index";
 import { Provider } from "react-redux";
 
+// sentry error tracker
+import * as Sentry from "@sentry/react"
+// Google analytics
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-9P213XVE0W");
+
+Sentry.init({
+  dsn: "https://f53bc790709548b6a11dcb036e266084@o4505419581816832.ingest.sentry.io/4505419586011136",
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    }),
+    new Sentry.Replay(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 // functional component
 function App() {
   const maintenance = [""]
@@ -65,10 +88,13 @@ function App() {
   />
   } else {
     // JSX
-    return <Provider store={store}>
+    return (
+      <>
+    <Provider store={store}>
       <RouterProvider router={router} />
     </Provider> 
-  }
+      </>
+  )}
 }
 
 export default App
